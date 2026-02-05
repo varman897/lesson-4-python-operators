b3f8ddc608ded620aa7be62f8dc988c6803cb0d1
varman897 <varmansivarooban2015@gmail.com>
2026-02-04 19:51:12 -0800

    moneyy

---
+++ money.py
@@ -0,0 +1,9 @@
+Amount=int(input("please enter the amount for withdrawal: "))
+note_1=Amount//100  
+note_2=(Amount%100)//50
+note_3=((Amount%100)%50)//10
+
+
+print("notes of 100 dollars:",note_1)
+print("notes of 50 dollars:",note_2)
+print("notes of 10 dollars:",note_3)
