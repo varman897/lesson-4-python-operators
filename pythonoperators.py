3de58d1d0f6b4108b808f43c8456bc51b8cd078d
varman897 <varmansivarooban2015@gmail.com>
2026-02-04 19:40:46 -0800

    python operators

---
+++ pythonoperators.py
@@ -0,0 +1,10 @@
+tree1=91
+tree2=57
+tree3=43
+tree4=78
+tree5=67
+
+sum = tree1 + tree2 + tree3 + tree4 + tree5
+print("the sum of all the trees is:", sum)
+average=sum/5
+print("the average of all the tree is:", average)
